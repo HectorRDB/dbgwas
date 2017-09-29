6fb5e4c8c74caa8af9303ecf8488d0c530c2c254
Leandro Ishi Soares de Lima <leandro.ishi.lima@gmail.com>
2017-09-29 21:16:31 +0200

    Added a new parameter: -maf
    -Added a new parameter: -maf
        -Minor allele frequency filter, defaults to 0.1
    
    
    Former-commit-id: 930efaa5bde3b08ef565c80a10bcdf424125e573

--- tools/DBGWAS/src/global.cpp
+++ tools/DBGWAS/src/global.cpp
@@ -46,6 +46,7 @@ const char* STR_NEWICK_PATH = "-newick";
 const char* STR_SFF = "-SFF";
 const char* STR_NUCLEOTIDE_DB = "-nc_db";
 const char* STR_PROTEIN_DB = "-pt_db";
+const char* STR_MAF_FILTER = "-maf";
 
 //TODO: seeveral questions are still unclear if we use the Freq count mode (how to run bugwas, the coloring, etc...). For now I am disabling this option
 //const char* STR_COUNT_MODE = "-count";
@@ -74,6 +75,7 @@ void populateParser (Tool *tool) {
 
   tool->getParser()->push_front (new OptionOneParam (STR_KSKMER_SIZE, "K-mer size",  false, "31"));
   tool->getParser()->push_front (new OptionOneParam (STR_NBCORES, "Number of cores",  false, "1"));
+  tool->getParser()->push_front (new OptionOneParam (STR_MAF_FILTER, "Minor Allele Frequency Filter",  false, "0.01"));
   tool->getParser()->push_front (new OptionOneParam (STR_STRAINS_FILE, "A text file describing the strains containing 3 collumns: 1) ID of the strain; 2) Phenotype (0/1/NA); 3) Path to a multi-fasta file containing the sequences of the strain. This file needs a header. Check the sample_example folder for an example.",  true));
   tool->getParser()->push_front (new OptionOneParam (STR_NEWICK_PATH, "Path to a newick tree file",  true));
   tool->getParser()->push_front (new OptionOneParam (STR_MAX_NEIGHBOURHOOD, "Denotes the maximum neighbourhood that can be viewed in the final visualization",  false, "5"));
