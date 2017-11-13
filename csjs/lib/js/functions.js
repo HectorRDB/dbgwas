f6f34564f7525f1e8ae9361ca87c2404625acfd9
Leandro Ishi Soares de Lima <leandro.ishi.lima@gmail.com>
2017-11-13 16:55:08 +0100

    Fixed Firefox bug on modal positioning
    Former-commit-id: c811dcb33210d343b4c6cf516c0851de7c1a8770

--- csjs/lib/js/functions.js
+++ csjs/lib/js/functions.js
@@ -305,7 +305,7 @@ function makeFile (text, file, fileType) {
 //FUNCTIONS FOR CUSTOM RENDERING ON HANDSONTABLE
 function showFullString (event, longString) {
     $("<div>").html("<textarea class=\"code\" rows=\"10\" style=\"width: 100%\" readonly>"+ longString + "</textarea>").dialog({
-        position: {my: "left top", at: "left bottom", of: event.srcElement},
+        position: {my: "left top", at: "left bottom", of: event},
         close: function() {
             $(this).dialog('destroy').remove();
         }
@@ -358,7 +358,7 @@ function showAnnotationTableOfNode (event, title, nodeId) {
     $("<div>").html("<div class=\"nodeAnnotationTable\" id=\"nodeAnnotationDiv_"+randomId+"\"></div>").dialog({
         title: title,
         width: 400,
-        position: {my: "left top", at: "left bottom", of: event.srcElement},
+        position: {my: "left top", at: "left bottom", of: event},
         close: function() {
             $(this).dialog('destroy').remove();
         }
