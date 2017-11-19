ffe11d82b04da020a57b2c2d7c963edbb6e15e08
Leandro Ishi Soares de Lima <leandro.ishi.lima@gmail.com>
2017-11-19 17:28:58 +0100

    Working on DBGWAS now processes a "DBGWAS_extra_tag_xyz = <value>;" tag
    Former-commit-id: b1b51fd5eefd39f368da89be708d95e356932f3d

--- tools/DBGWAS/src/main.cpp
+++ tools/DBGWAS/src/main.cpp
@@ -45,7 +45,8 @@ using namespace std;
 
 int main (int argc, char* argv[])
 {
-    static const regex expression("DBGWAS_index_tag \\s* = \\s* (\\w+) \\s*;");
+    //static const regex expression("DBGWAS_index_tag \\s* = \\s* (\\w+) \\s*;");
+    static const regex expression("(\\w+)");
     string s = ">sp|P27431|ROXA_ECOLI;DBGWAS_index_tag=ROXA;DBGWAS_graph_tag=[uniprot] 50S ribosomal protein L16 3-hydroxylase (Escherichia coli)";
     smatch matchResults;
     regex_search(s, matchResults, expression);
