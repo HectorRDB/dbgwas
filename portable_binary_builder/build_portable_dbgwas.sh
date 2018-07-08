b5febfc9639e08a49b904abf22d4f973c9254aa3
Leandro Ishi <leandro.ishi.lima@gmail.com>
2018-07-09 00:12:08 +0200

    Small fix to portable_binary_builder/build_portable_dbgwas.sh

--- portable_binary_builder/build_portable_dbgwas.sh
+++ portable_binary_builder/build_portable_dbgwas.sh
@@ -3,10 +3,10 @@
 # on how he and the other authors managed to make kallisto (Nicolas L Bray, Harold Pimentel, Páll Melsted and Lior Pachter,
 # Near-optimal probabilistic RNA-seq quantification, Nature Biotechnology 34, 525–527 (2016), doi:10.1038/nbt.3519)
 # portable in different linux distributions.
-
-git clone --recursive https://gitlab.com/leoisl/dbgwas.git
-
+set -eux
+cd ..
+rm -rf build
 sudo docker run -t -i --rm \
   -v `pwd`:/io \
   phusion/holy-build-box-64:latest \
-  bash /io/build_portable_dbgwas_core.sh
+  bash /io/portable_binary_builder/build_portable_dbgwas_core.sh
