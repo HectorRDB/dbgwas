711ab44e666d1045742a324899deeb0b8fd0160b
Leandro Ishi Soares de Lima <leandro.ishi.lima@gmail.com>
2018-07-12 00:12:53 +0200

    Updating tests/test_DBGWAS_on_pseudomonas_aeruginosa.sh

--- tests/test_DBGWAS_on_pseudomonas_aeruginosa.sh
+++ tests/test_DBGWAS_on_pseudomonas_aeruginosa.sh
@@ -5,7 +5,7 @@ wget https://www.dropbox.com/s/0g1llvdbfv1jys6/pseudomonas_aeruginosa_full_datas
 unzip pseudomonas_aeruginosa_full_dataset.zip
 wget https://www.dropbox.com/s/mt3g4oh0bt5jwmr/Resistance_DB_for_DBGWAS.fasta?dl=1 -O Resistance_DB_for_DBGWAS.fasta
 wget https://www.dropbox.com/s/9y1p0yw918ips6k/uniprot_sprot_bacteria_for_DBGWAS.fasta?dl=1 -O uniprot_sprot_bacteria_for_DBGWAS.fasta
-./DBGWAS -strains pseudomonas_aeruginosa_full_dataset/strains -newick pseudomonas_aeruginosa_full_dataset/strains.newick -nc_db Resistance_DB_for_DBGWAS.fasta -pt_db uniprot_sprot_bacteria_for_DBGWAS.fasta
+./DBGWAS -strains pseudomonas_aeruginosa_full_dataset/strains -newick pseudomonas_aeruginosa_full_dataset/strains.newick -nc-db Resistance_DB_for_DBGWAS.fasta -pt-db uniprot_sprot_bacteria_for_DBGWAS.fasta
 
 #get the correct output
 wget https://www.dropbox.com/s/jp24184zony1977/correct_output.zip?dl=1 -O correct_output.zip
