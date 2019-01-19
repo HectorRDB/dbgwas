7a4afa7df836612f133831a63bcf24a6adddb109
Leandro Ishi Soares de Lima <leandro.ishi.lima@gmail.com>
2019-01-19 14:20:38 +0100

    Updating docker dev

--- containers/docker_dev/start_dbgwas_dev_env.sh
+++ containers/docker_dev/start_dbgwas_dev_env.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
+#just a small shortcut to docker run
 
-#get the dbgwas dir
+#get the script and dbgwas dir
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 dbgwasDir="$(dirname "$(dirname "$scriptDir")")"
 
-#pull the image - we fixed at version 2.0.1 so that we are always stable
-docker pull phusion/holy-build-box-64:2.0.1
-
 #run the container
 #this will start the container and put you in the dbgwas folder, ready for you to work
-docker run --rm -it -v ${dbgwasDir}:/dbgwas phusion/holy-build-box-64:2.0.1 /bin/bash -c "cd /dbgwas && source /hbb_exe/activate && /bin/bash"
+docker run --rm -it -v ${dbgwasDir}:/dbgwas leandroishilima/dbgwas:dev_0.5.4
 
 #you can proceed by doing the common compilation commands:
 #mkdir build && cd build && cmake .. && make
@@ -17,3 +15,5 @@ docker run --rm -it -v ${dbgwasDir}:/dbgwas phusion/holy-build-box-64:2.0.1 /bin
 #note that the first compilation will take a lot of time. The next ones will be only incremental
 #do not delete anything after finishing your work, just exit the container
 #this is made so that the next time you work, the compilation is incremental
+
+#this container can also run DBGWAS (it has R and the required packages installed)
